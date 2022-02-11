13517e9c9be8777ee8fe6d8e42c5be8a5649e2da
siddik <spolatde@gmail.com>
2022-02-11 22:10:21 +0100

    in command there is  a new func

--- mongoDb/index.js
+++ mongoDb/index.js
@@ -28,3 +28,34 @@ app.listen(3000, () => {
 
 
 export {client}
+
+
+/* export async function deleteOne(id) {
+  try {
+      const objId= new ObjectId(id);
+      // Verbunden mit EINER INSTANZ
+      await client.connect();
+      // Wähle eine Datenbank
+      const db = client.db('test-2');
+      // Wähle Collection
+      const collection = db.collection('animals');
+
+      const deletedAnimal = getOne(id)
+
+      await collection.deleteOne(objId)
+
+      return deletedAnimal;
+      
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      client.close();
+  }
+} */
+
+/* app.delete('/animals/:id', async(req, res)=>{
+  const id=req.params
+  const result = await deleteOne(id);
+  res.send(`${result} wurde gelöscht`)
+}); */
