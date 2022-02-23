aa8feeb109aed1dbd32663988480d7bb92f848f5
siddik <spolatde@gmail.com>
2022-02-23 22:42:49 +0100

    orderscontroller created

---
+++ record_shop_fullstack/record_shop/controller/orders.controller.js
@@ -0,0 +1,46 @@
+import Order from '../models/order.model.js';
+
+export const getOrders = async (req, res) => {
+    const orders = await Order.find();
+    res.json(orders);
+};
+
+export const getOrderById = async (req, res) => {
+    const { id } = req.params;
+
+    const order = await Order.findById(id);
+    if (!order) {
+        return res.status(400).send('Nicht gefunden');
+    }
+    
+    res.json(order);
+};
+
+export const addOrder = async (req, res) => {
+    const data = req.body;
+    // Testen ob data alle infos enthält: title, artist, year, cover, price
+    if (!data.recordId || !data.userId || !data.quantity) {
+        return res.status(400).send('Felende Daten');
+    }
+    
+    const order = new Order({
+        recordId: data.recordId,
+        userId: data.userId,
+        quantity: data.quantity,
+    });
+
+    await order.save();
+
+    res.send(order);
+}
+
+export const deleteOrder = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const result = await Order.deleteOne({ _id: id });
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send('Nicht gefunden mit id: '+id+' - '+err);
+    }
+}
