7fe6258f764d908d73baf078e14e8c340205a6d8
siddik <spolatde@gmail.com>
2022-02-23 22:43:39 +0100

    userscontroller created

---
+++ record_shop_fullstack/record_shop/controller/users.controller.js
@@ -0,0 +1,47 @@
+import db from '../database.js';
+import User from '../models/user.model.js'
+
+export const getUsers = async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+};
+
+export const getUserById = async (req, res) => {
+    const { id } = req.params;
+
+    const user = await User.findById(id);
+    if (!user) {
+        return res.status(400).send('Nicht gefunden');
+    }
+    
+    res.json(user);
+};
+
+export const addUser = async (req, res) => {
+    const data = req.body;
+    // Testen ob data alle infos enthält: title, artist, year, cover, price
+    if (!data.firstName || !data.lastName || !data.email || !data.password) {
+        return res.status(400).send('Falsche Daten');
+    }
+    
+    const user = new User({
+        firstName: data.firstName,
+        lastName: data.lastName,
+        email: data.email,
+        password: data.password,
+    });
+
+    await user.save();
+
+    res.send(user);
+}
+
+export const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await User.deleteOne({ _id: id });
+        res.json(result);
+    } catch (err) {
+        return res.status(400).send('Nicht gefunden mit id: '+id+' - '+err);
+    }
+}
