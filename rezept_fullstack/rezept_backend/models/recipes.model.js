cf88e2f26b13d847f3a3742e706818b0e751aa86
siddik <spolatde@gmail.com>
2022-03-01 21:11:57 +0100

    recipe model wurde erneuert

--- rezept_fullstack/rezept_backend/models/recipes.model.js
+++ rezept_fullstack/rezept_backend/models/recipes.model.js
@@ -1,29 +1,38 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const { model, Schema } = mongoose;
+const User = require("./users.model");
 
 const recipesSchema = new Schema({
-   title: { require: true, type: String },
-   image: {
-      required: false,
-      type: String,
-   },
-   description: {
-      required: true,
-      type: String,
-   },
-   ingredients: {
-      required: true,
-      type: Object,
-   },
-   category: {
-      required: true,
-      type: Array,
-   },
-   preparationTime: {
-      required: true,
-      type: String,
-   },
+  title: { require: true, type: String },
+  image: {
+    required: false,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  ingredients: {
+    required: true,
+    type: Object,
+  },
+  category: {
+    required: true,
+    type: Array,
+  },
+  preparationTime: {
+    required: true,
+    type: String,
+  },
+  rating: {
+    required: true,
+    type: Number,
+  },
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+  },
 });
 
-const Recipe = model('Recipe', recipesSchema);
+const Recipe = model("Recipe", recipesSchema);
 module.exports = Recipe;
