aa679b6f4046c2e27d409114675b195c30a3621c
siddik <spolatde@gmail.com>
2022-03-05 22:46:01 +0100

    new user send to db

--- udemy_kurs/mongodb/index.js
+++ udemy_kurs/mongodb/index.js
@@ -1,4 +1,4 @@
-const mongodb = required('mongodb')
+const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient;
 require("dotenv").config();
 
@@ -6,3 +6,23 @@ require("dotenv").config();
 const uri = process.env.MONGODB
 
 
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, async(error, result)=>{
+
+    if (error) {
+       return  console.log("Can not be connected with DB" + error);
+    }
+
+    console.log("Connected with DB");
+    const db = result.db('udemy_mongodb');
+
+   await db.collection('users').insertOne({
+        name:'Hakan',
+        age:32,
+        isEmployee:true
+    
+    }).then(result =>  console.log(result)
+       
+    ).catch(error => console.log(error))
+
+
+})
