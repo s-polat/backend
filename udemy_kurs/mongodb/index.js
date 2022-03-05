bf792e87b69c5d9d2da51476f327d417042b8df2
siddik <spolatde@gmail.com>
2022-03-06 00:00:01 +0100

    a

--- udemy_kurs/mongodb/index.js
+++ udemy_kurs/mongodb/index.js
@@ -1,12 +1,10 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient;
+const { MongoClient } = require ("mongodb");
 require("dotenv").config();
-
 
 const uri = process.env.MONGODB
 
-
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, async(error, result)=>{
+//first method
+/* MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, async(error, result)=>{
 
     if (error) {
        return  console.log("Can not be connected with DB" + error);
@@ -25,4 +23,20 @@ MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, as
     ).catch(error => console.log(error))
 
 
-})
+}) */
+let db;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(async(err, result) => {
+    if(err){return console.log("Cannot connected to DB");}
+    
+    db=await result.db('udemy_mongodb')
+    console.log('Connected to DB');
+});
+console.log(db);
+
+const collection = db.collection("users");
+
+collection.insertOne({
+    name:"Mehmet",
+    age:38
+}).try(result => console.log(result)).catch(error => console.log(error))
