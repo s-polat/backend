7cce900dbfac9f7b58c1d7be4db4d712e89db5e1
siddik <spolatde@gmail.com>
2022-03-14 22:30:02 +0100

    permission added in record router

--- record_shop_fullstack/record_shop/routes/records.routes.js
+++ record_shop_fullstack/record_shop/routes/records.routes.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { getRecords, addRecord, getRecordById, deleteRecord } from '../controller/records.controller.js';
+import { permission } from '../middleware/PermissionMiddleware.js';
 
 const router = new Router();
+router.use(permission())
 
 router.route('/')
     .get(getRecords)
